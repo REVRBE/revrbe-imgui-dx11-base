7ec79cb73bc30a72b2872db324561829ce8e91d7
REVRBE <129604052+REVRBE@users.noreply.github.com>
2023-07-17 14:16:15 +0200

    Update main.cpp

--- revrbe-imgui-dx11-base/revrbe-imgui-dx11-base/main.cpp
+++ revrbe-imgui-dx11-base/revrbe-imgui-dx11-base/main.cpp
@@ -198,44 +198,8 @@ void SetupImGui(HWND hWnd) {
     style.ChildRounding = 5;
     style.FrameRounding = 5;
 
-    ImGui::StyleColorsDark();
-    style.Colors[ImGuiCol_Text] = ImVec4(0.85f, 0.92f, 0.95f, 0.78f);
-    style.Colors[ImGuiCol_TextDisabled] = ImVec4(0.85f, 0.92f, 0.95f, 0.28f);
-    style.Colors[ImGuiCol_WindowBg] = ImVec4(0.10f, 0.18f, 0.23f, 1.00f);
-    style.Colors[ImGuiCol_Border] = ImVec4(0.31f, 0.48f, 0.69f, 0.00f);
-    style.Colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-    style.Colors[ImGuiCol_FrameBg] = ImVec4(0.20f, 0.32f, 0.42f, 1.00f);
-    style.Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.78f);
-    style.Colors[ImGuiCol_FrameBgActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_TitleBg] = ImVec4(0.10f, 0.18f, 0.23f, 1.00f);
-    style.Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.10f, 0.18f, 0.23f, 0.75f);
-    style.Colors[ImGuiCol_TitleBgActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_MenuBarBg] = ImVec4(0.10f, 0.18f, 0.23f, 0.47f);
-    style.Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.10f, 0.18f, 0.23f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.48f, 0.69f, 1.00f);
-    style.Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.78f);
-    style.Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_CheckMark] = ImVec4(0.31f, 0.48f, 0.69f, 1.00f);
-    style.Colors[ImGuiCol_SliderGrab] = ImVec4(0.35f, 0.63f, 0.82f, 0.14f);
-    style.Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_Button] = ImVec4(0.35f, 0.63f, 0.82f, 0.14f);
-    style.Colors[ImGuiCol_ButtonHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.86f);
-    style.Colors[ImGuiCol_ButtonActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_Header] = ImVec4(0.44f, 0.60f, 0.75f, 0.76f);
-    style.Colors[ImGuiCol_HeaderHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.86f);
-    style.Colors[ImGuiCol_HeaderActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_Separator] = ImVec4(0.22f, 0.38f, 0.51f, 1.00f);
-    style.Colors[ImGuiCol_SeparatorHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.78f);
-    style.Colors[ImGuiCol_SeparatorActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_ResizeGrip] = ImVec4(0.35f, 0.63f, 0.82f, 0.04f);
-    style.Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.44f, 0.60f, 0.75f, 0.78f);
-    style.Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_PlotLines] = ImVec4(0.85f, 0.92f, 0.95f, 0.63f);
-    style.Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.85f, 0.92f, 0.95f, 0.63f);
-    style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.44f, 0.60f, 0.75f, 1.00f);
-    style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.44f, 0.60f, 0.75f, 0.43f);
-    style.Colors[ImGuiCol_PopupBg] = ImVec4(0.10f, 0.18f, 0.23f, 0.9f);
+    const char* standardColors = "Standard";
+    gui->ApplyColorScheme(standardColors);
 
     // Load Fonts
     // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
